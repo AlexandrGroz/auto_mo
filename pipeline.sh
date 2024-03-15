3a2fe1d08880da304c4114db13dbbb32d8fffe9b
r.m.gazizulin <r.m.gazizulin@tmn3.etagi.com>
2024-03-15 23:29:16 +0500

    python для windows

--- pipeline.sh
+++ pipeline.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Создать датасет
-python3 data_creation.py
+python data_creation.py
 
 # Предобрабатываем данные
-python3 model_preprocessing.py
+python model_preprocessing.py
 
 # Обучение модели
-python3 model_preparation.py
+python model_preparation.py
 
 # Проверка модели
-python3 model_testing.py
+python model_testing.py
